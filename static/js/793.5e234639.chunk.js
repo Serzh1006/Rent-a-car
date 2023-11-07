187396b40819e1a9a065db2a57ca9772e749b6b4
Serzh1006 <skravchenko223@gmail.com>
2023-11-07 11:50:44 +0000

    Deploying to gh-pages from @ Serzh1006/Rent-a-car@b6883809a5051bb1467fd10bb9a93c1f069ddbd6 🚀

---
+++ static/js/793.5e234639.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[793],{7793:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"Favorite"})}}}]);
+//# sourceMappingURL=793.5e234639.chunk.js.map
