d29383faa7903dc530104a8fe4fbd96a94a4890a
Serzh1006 <skravchenko223@gmail.com>
2023-11-30 12:26:02 +0100

    add comments

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -1,42 +1,42 @@
 // import Select from 'react-select';
-import { Select } from '@chakra-ui/react';
-import { useState } from 'react';
+// import { Select } from '@chakra-ui/react';
+// import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { filterByName } from '../../redux/filterSlice';
-import css from './select.module.css';
+// import css from './select.module.css';
 import { useEffect } from 'react';
 
-const options = [
-  { value: 'buick', label: 'Buick' },
-  { value: 'volvo', label: 'Volvo' },
-  { value: 'hummer', label: 'HUMMER' },
-  { value: 'subaru', label: 'Subaru' },
-  { value: 'mitsubishi', label: 'Mitsubishi' },
-  { value: 'nissan', label: 'Nissan' },
-  { value: 'lincoln', label: 'Lincoln' },
-  { value: 'gmc', label: 'GMC' },
-  { value: 'hyundai', label: 'Hyundai' },
-  { value: 'mini', label: 'MINI' },
-  { value: 'bentley', label: 'Bentley' },
-  { value: 'mercedes-benz', label: 'Mercedes-Benz' },
-  { value: 'aston martin', label: 'Aston Martin' },
-  { value: 'pontiac', label: 'Pontiac' },
-  { value: 'lamborghini', label: 'Lamborghini' },
-  { value: 'audi', label: 'Audi' },
-  { value: 'bmw', label: 'BMW' },
-  { value: 'chevrolet', label: 'Chevrolet' },
-  { value: 'chrysler', label: 'Chrysler' },
-  { value: 'kia', label: 'Kia' },
-  { value: 'land rover', label: 'Land Rover' },
-];
+// const options = [
+//   { value: 'buick', label: 'Buick' },
+//   { value: 'volvo', label: 'Volvo' },
+//   { value: 'hummer', label: 'HUMMER' },
+//   { value: 'subaru', label: 'Subaru' },
+//   { value: 'mitsubishi', label: 'Mitsubishi' },
+//   { value: 'nissan', label: 'Nissan' },
+//   { value: 'lincoln', label: 'Lincoln' },
+//   { value: 'gmc', label: 'GMC' },
+//   { value: 'hyundai', label: 'Hyundai' },
+//   { value: 'mini', label: 'MINI' },
+//   { value: 'bentley', label: 'Bentley' },
+//   { value: 'mercedes-benz', label: 'Mercedes-Benz' },
+//   { value: 'aston martin', label: 'Aston Martin' },
+//   { value: 'pontiac', label: 'Pontiac' },
+//   { value: 'lamborghini', label: 'Lamborghini' },
+//   { value: 'audi', label: 'Audi' },
+//   { value: 'bmw', label: 'BMW' },
+//   { value: 'chevrolet', label: 'Chevrolet' },
+//   { value: 'chrysler', label: 'Chrysler' },
+//   { value: 'kia', label: 'Kia' },
+//   { value: 'land rover', label: 'Land Rover' },
+// ];
 
 const CarsList = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  // const [selectedOption, setSelectedOption] = useState(null);
   const dispatch = useDispatch();
 
-  const getValueFromList = () => {
-    dispatch(filterByName(selectedOption.value));
-  };
+  // const getValueFromList = () => {
+  //   dispatch(filterByName(selectedOption.value));
+  // };
 
   useEffect(() => {
     dispatch(filterByName(''));
